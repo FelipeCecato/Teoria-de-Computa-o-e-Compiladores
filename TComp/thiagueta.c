d21ed976b8af75cfe88d8044396cded56fabc4c2
Felipe Cecato <108101515+FelipeCecato@users.noreply.github.com>
2024-05-11 20:38:40 -0300

    Add files via upload

---
+++ TComp/thiagueta.c
@@ -0,0 +1,23 @@
+#include<stdio.h> 
+#include<stdlib.h>
+int verif_ident(FILE *arq){
+    char c;
+    int cont = 0;
+    do{
+        c = fgetc(arq)
+        cont++;
+    }while(c!=Especial)
+    f
+
+}
+
+
+
+
+int main(int argc, char **argv){
+
+
+
+
+
+}
