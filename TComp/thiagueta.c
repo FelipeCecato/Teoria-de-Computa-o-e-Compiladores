87405368b94578033bb6d4000158b0e5fb5649d0
Felipe Cecato <108101515+FelipeCecato@users.noreply.github.com>
2024-05-11 21:35:26 -0300

    Update thiagueta.c

--- TComp/thiagueta.c
+++ TComp/thiagueta.c
@@ -4,8 +4,17 @@
 #define Numero ( c>= '0' && c<='9' ) // Numeros de 0 a 9
 #define Letra ( c>='A' && c<='z' ) // Letras do alfabeto
 #define CaracterEspecial ( c>='(' && c <= '/') // Caracteres permitidos no PL0
+#define PalavraEspecial !strcmp(retorno,"BEGIN")||!strcmp(retorno,"VAR")||!strcmp(retorno,"END")||!strcmp(retorno,"ODD")||!strcmp(retorno,"CALL")||!strcmp(retorno,"IF")||!strcmp(retorno,"WHILE")||!strcmp(retorno,"IF")||!strcmp(retorno,"THEN")||!strcmp(retorno,"DO")||!strcmp(retorno,"PROCEDURE")
 
-char *verif_ident(FILE *arq){ // Verifica se o que esta lendo eh ident
+/* Verifica se eh palavra reservada */
+int verificar_palavras_reservadas(retorno){
+    if(PalavraEspecial)
+        return 1;
+    return 0;
+}
+
+/* Verifica se o que esta lendo eh ident */
+char *verif_ident(FILE *arq){ 
 
     int cont = 0;
     char *retorno;    
@@ -20,9 +29,11 @@ char *verif_ident(FILE *arq){ // Verifica se o que esta lendo eh ident
 
     fseek(arq,SEEK_CUR,-1);
 
-    if(Numero||Letra||Especial){
-        printf("%s, ident\n", retorno);
-
+    if(Especial){
+        if(verificar_palavras_reservadas(retorno))
+            printf("%s, %s", retorno)
+        else
+            printf("%s, ident\n", retorno);
     }
 
     else if (c != EOF)
@@ -31,9 +42,6 @@ char *verif_ident(FILE *arq){ // Verifica se o que esta lendo eh ident
         printf("%s, <ERRO_LEXICO>\n", retorno);
 
 }
-
-
-
 
 int main(int argc, char **argv){
 
