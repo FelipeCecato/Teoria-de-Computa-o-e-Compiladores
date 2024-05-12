dddb0d0985a24678aba63a96cc10dd1cdf348412
briotisk <briotisk@gmail.com>
2024-05-12 11:24:25 -0300

    falta verificar se funciona para comentários, erros lexicos, adicionar verificação de palavras reservadas e testar para o símbolo de igual

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ executar:
 
 ver resultado:
     cat saida.txt
+
+No autômato: criar um estado final 22 com uma transição do estado 21 para 22 ao se ler "outro"
+No autômato: criar um estado final 23 com uma transição do estado 0 para 23 ao se ler ";"
--- TComp/main.c
+++ TComp/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "msgnsErro.h"
 
 #define undefined -1
@@ -8,8 +9,17 @@
 #define true 1
 #define false 0
 
+char pertence_alfabeto(char c) {
+    
+    if(isalnum(c) || (c > 41 && c < 48 && c != 44) || (c > 57 && c < 63) )
+        return true;
+
+    return false;
+
+}
+
 //qual a melhor forma de implementar isso de transição? talvez usar uma matriz seja uma boa ideia
-char transicao(const char s, const char c) {
+char transicao(const char s, const char c, FILE *source_file) {
 
     if(s == 0) {
 
@@ -25,11 +35,102 @@ char transicao(const char s, const char c) {
         if(c == '*')
             return 20;
 
+        if(isdigit(c))
+            return 21;
+
         if(c == '.')
             return 14;
 
         if(c == '=')
             return 12;
+
+        if(c == ' ' || c == ',')
+            return 0;
+
+        if(c == '{')
+            return 1;
+        
+        //criar um novo estado para ; no automato
+        if(c == ';')
+            return 23;
+        
+        if(isalpha(c))
+            return 3;
+        
+        if(c == '<')
+            return 7;
+        
+        if(c == '>')
+            return 8;
+
+        if(c == ':')
+            return 16;
+        
+        //se não é nenhuma
+        //return <ERRO>
+    }
+
+    if(s == 1) {
+
+        //se identificar o fim do comentário avança para o estado 2, 
+        //caso contrário se mantém no estado 1
+        if(c == '}')
+            return 2;
+
+        return 1;
+    }
+
+    if(s == 3) {
+
+        //enquanto forem identificados letras ou dígitos ou '_' ele mantém o autômato no estado 3
+        if(isalnum(c) || c == '_')
+            return 3;
+
+        fseek(source_file, -1, SEEK_CUR);
+        return 4;
+        
+    }
+
+    if(s == 7) {
+        
+        if(c == '=')
+            return 11;
+        
+        if(c == '>')
+            return 5;
+        
+        fseek(source_file, -1, SEEK_CUR);
+        return 10;
+
+    }
+
+    if(s == 8) {
+
+        if(c == '=')
+            return 9;
+
+        fseek(source_file, -1, SEEK_CUR);
+        return 6;
+
+    }
+
+    if(s == 16) {
+
+        if(c == '=')
+            return 17;
+
+        fseek(source_file, -1, SEEK_CUR);
+        return 18;
+
+    }
+
+    if(s == 21) {
+
+        if(c == isdigit(c))
+            return 21;
+
+        fseek(source_file, -1, SEEK_CUR);
+        return 22;
     }
 
 }
@@ -41,14 +142,25 @@ char estado_final(char s) {
        s == 15 ||
        s == 19 ||
        s == 20 ||
-       s == 14)
+       s == 22 ||
+       s == 23 ||
+       s == 14 ||
+       s == 17 ||
+       s == 18 ||
+       s == 10 ||
+       s == 11 ||
+       s == 5 ||
+       s == 9 ||
+       s == 6 ||
+       s == 2 ||
+       s == 4)
         return true;
 
     return false;
 
 }
 
-void definir_classe(char *classe, const char s) {
+void definir_classe(char *classe, const char s, FILE *source_file) {
 
     switch (s) {
 
@@ -75,6 +187,50 @@ void definir_classe(char *classe, const char s) {
         case 20:
             strcpy(classe, "<simb_multiplicacao>");
             break;
+        case 18:
+            strcpy(classe, "<simb_dois_pts>");
+            break;
+
+        case 17:
+            strcpy(classe, "<simb_atribuicao>");
+            break;
+
+        case 10:
+            strcpy(classe, "<simb_menor>");
+            break;
+
+        case 5:
+            strcpy(classe, "<simb_diferente>");
+            break;
+
+        case 11:
+            strcpy(classe, "<simb_menor_igual>");
+            break;
+
+        case 6:
+            strcpy(classe, "<simb_maoir>");
+            break;
+        
+        case 9:
+            strcpy(classe, "<simb_maior_igual>");
+            break;
+
+        case 22:
+            strcpy(classe, "<numero>");
+            break;
+        
+        case 23:
+            strcpy(classe, "<simb_ponto_virgula>");
+            break;
+
+        case 2:
+            strcpy(classe, "<comentário>");
+            break;
+
+        case 4:
+            //verificar se não é palavra reservada
+            strcpy(classe, "<identificador>");
+            break;
 
         default://colocar <ERRO_LEXICO> aqui?
             break;
@@ -94,6 +250,7 @@ int main(int argc, char const *argv[]) {
     //copia o nome do arquivo e abre em modo leitura
     strcpy(filename, argv[1]);
     FILE *source_file;
+    source_file = fopen("teste.txt", "r");
     source_file = fopen(filename, "r");
     if(source_file == NULL){//imprime uma mensagem de erro caso a abertura do arquivo falhe
         ERRO_ARQUIVO
@@ -110,8 +267,7 @@ int main(int argc, char const *argv[]) {
 
     char *token;
     token = malloc(sizeof(char));
-    char *token_ini = token;
-    *token = '\n';
+    *token = '\0';
     if(token == NULL) {//imprime uma mensagem de erro caso a alocação falhe
         ERRO_ALOCACAO
         return -1; 
@@ -127,15 +283,16 @@ int main(int argc, char const *argv[]) {
     char c, s;
     c = fgetc(source_file);
     s = 0;
-
-
+    
     while (c != EOF) {
 
-        //adiciona o caractere c à cadeia do token
+        // se c, faz parte do alfabeto da linguagem, adiciona o caractere c à cadeia do token
         token = realloc(token, (strlen(token)+1)*sizeof(char));
-        strncat(token, &c, 1);
 
-        s = transicao(s,c);
+        s = transicao(s,c, source_file);
+
+        if(s != 1 && s != 4 && s != 22 && pertence_alfabeto(c))
+            strncat(token, &c, 1);
 
         if(s == undefined) {
 
@@ -145,18 +302,24 @@ int main(int argc, char const *argv[]) {
             fwrite(classe, sizeof(char)*strlen(classe), 1, output_file);
             fwrite("\n", sizeof(char)*strlen("\n"), 1, output_file);
             s = 0;
-            token = realloc(token, sizeof(char));
+            token = realloc(token, sizeof(char)*2);
             strcpy(token, "");
 
         }
 
         if(estado_final(s)) {
 
-            definir_classe(classe, s);
-            fwrite(token, sizeof(char)*strlen(token), 1, output_file);
-            fwrite(", ", sizeof(char)*strlen(", "), 1, output_file);
-            fwrite(classe, sizeof(char)*strlen(classe), 1, output_file);
-            fwrite("\n", sizeof(char)*strlen("\n"), 1, output_file);
+            definir_classe(classe, s, source_file);
+
+            if(strcmp(classe, "<comentario>")) {
+
+                fwrite(token, sizeof(char)*strlen(token), 1, output_file);
+                fwrite(", ", sizeof(char)*strlen(", "), 1, output_file);
+                fwrite(classe, sizeof(char)*strlen(classe), 1, output_file);
+                fwrite("\n", sizeof(char)*strlen("\n"), 1, output_file);
+            
+            }
+            
             s = 0;
             token = realloc(token, sizeof(char));
             strcpy(token, "");
@@ -169,7 +332,6 @@ int main(int argc, char const *argv[]) {
     
     //libera memória 
     free(filename);
-    free(output_file);
     free(token);
     free(classe);
 
