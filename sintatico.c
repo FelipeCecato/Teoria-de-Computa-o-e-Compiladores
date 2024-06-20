8e62ded45b23caa445099c305772ad91369f1baa
FelipeCecato <fececato31@gmail.com>
2024-06-20 15:46:25 -0300

    Adicionado novos prototipos

--- sintatico.c
+++ sintatico.c
@@ -19,16 +19,29 @@ int analise_ident(char *token){
 
 }
 
-int analise_VAR(){
-	
-	if(analise_ident()){
-		if(token == ;)`
+int analise_variavel(arq , token){
+	if(token == VAR)
+		if(analise_ident()){
+			if(token == ;)`
+				return 1;
+			return 0;
+		}
+
+	return 1;
+}
+
+int analise_procedimento(arq,token){
+	if(token == PROCEDURE){
+		automato(token);
+		if(token == ident)
 			return 1;
 		return 0;
 	}
-
-	return 0;
+	if(analise_bloco())
+	return 1;
 }
+
+
 
 int analise_sintatica(char *token){
 	
@@ -40,7 +53,20 @@ int analise_sintatica(char *token){
 
 
 }
+int analise_declaracao(){
+	if(analise_constante()&&analise_variavel()&&analise_procedimento)
+		return 1;
+	return 0;
+}
 
+
+
+int analise_bloco(){
+	if(analise_declaracao()&&analise_comando)
+		return 1;
+	
+	return 0;
+}
 
 int main(int argc, char const *argv[]) {
 
